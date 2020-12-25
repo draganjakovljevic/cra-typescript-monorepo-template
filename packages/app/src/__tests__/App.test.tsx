0fa5e97a746e854a3cf6888ba7e0b4b697500489
djakovljevic <djakovljevic@ebay.com>
2020-12-25 17:14:45 +0100

    run workspace tests from root

--- packages/app/src/__tests__/App.test.tsx
+++ packages/app/src/__tests__/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders shared component package', () => {
+test('renders text from shared component package', () => {
   render(<App />);
   const textElement = screen.getByText('Shared component package');
   expect(textElement).toBeInTheDocument();
